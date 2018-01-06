7a83fbc9d78d74bb908f4c178e2a88cae0cfb7cb
flo <f.muench@hotmail.com>
2018-01-06 18:54:55 +0100

    Refactor datatable data type to 'Invoice'

--- Frontend/src/app/invoices/invoices.component.ts
+++ Frontend/src/app/invoices/invoices.component.ts
@@ -11,8 +11,7 @@ import { Invoice } from '../shared/invoice-api/invoice.model';
 export class InvoicesComponent implements OnInit{
 
   public displayedColums;
-  public dataSource: MatTableDataSource<Element>;
-  // public invoices:  Element[] = new Array;
+  public dataSource: MatTableDataSource<Invoice>;
 
   constructor(
     private invoiceApi: InvoiceApiService
@@ -22,29 +21,9 @@ export class InvoicesComponent implements OnInit{
 
   ngOnInit() {
     this.displayedColums = [ 'date', 'user', 'amount'];
-    // this.dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
     this.invoiceApi.getInvoices().subscribe(data => {
-      let invoices = new Array<Element>();
-      data.forEach((data_row: Invoice) => {
-        invoices.push(<Element>{ date: data_row.Date,
-                                      user: data_row.User,
-                                      amount: data_row.Amount
-                                    });
-      });
-      this.dataSource = new MatTableDataSource<Element>(invoices);
+      this.dataSource = new MatTableDataSource<Invoice>(data);
     });
   }
 
 }
-
-export interface Element {
-  date: Date;
-  user: string;
-  amount: number;
-}
-
-const ELEMENT_DATA: Element[] = [
-  {date: new Date(), user: 'Hydrogen', amount: 1.0079},
-  {date: new Date(), user: 'Helium', amount: 4.0026},
-  {date: new Date(), user: 'Lithium', amount: 6.941}
-];
--- Frontend/src/app/shared/invoice-api/invoice.model.ts
+++ Frontend/src/app/shared/invoice-api/invoice.model.ts
@@ -11,3 +11,4 @@ export class Invoice {
         this.Amount = amount;
     }
 }
+
