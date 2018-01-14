0ad1feb35ba4bd3c0e26b2884dfe5e14f5e59ba8
flo <f.muench@hotmail.com>
2018-01-14 14:50:39 +0100

    Update models

--- Frontend/src/app/shared/balance-api/balance.model.ts
+++ Frontend/src/app/shared/balance-api/balance.model.ts
@@ -3,14 +3,22 @@ export class Balance {
     public StartDate: Date;
     public EndDate: Date;
     public TotalAmount: Number;
+    public AveragePartialAmount: Number;
     public Attendees: Array<BalanceAttendee>;
+    public BalanceFragments: Array<BalanceFragment>;
 }
 
 export class BalanceAttendee {
     public User: string;
     public PartialAmount: number;
+    public BalanceFragments: number;
 }
 
+export class BalanceFragment {
+    public PartialTransferAmount: number;
+    public ReceivingUser: string;
+    public SendingUser: string;
+}
 export class BalanceRequest {
     public StartDate = new Date().getUTCDate();
     public EndDate = new Date().getUTCDate();
