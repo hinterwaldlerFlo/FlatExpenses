82ce5c86e3ab7b6676258e07ef4293ff812a01c4
flo <f.muench@hotmail.com>
2018-01-14 14:50:58 +0100

    Extract balance-detail.component.html

--- Frontend/src/app/balance-detail/balance-detail.component.ts
+++ Frontend/src/app/balance-detail/balance-detail.component.ts
@@ -1,33 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatInputModule, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-balance-detail',
-  template: `
-  <div class="balance-detail-popup">
-    <h2>Abrechnung</h2>
-    <tr>
-      <td>Anfangsdatum:</td>
-      <td>{{ data.row.StartDate | date: 'dd.MM.yyyy' }}</td>
-    </tr>
-    <tr>
-      <td>Enddatum:</td>
-      <td>{{ data.row.EndDate | date: 'dd.MM.yyyy' }}</td>
-    </tr>
-    <table>
-      <ng-container *ngFor="let Attendee of data.row.Attendees">
-          <tr>
-            <td class="table-cell">{{Attendee.User}}</td>
-            <td class="table-cell">{{Attendee.PartialAmount}}</td>
-          </tr>
-      </ng-container>
-    </table>
-    <tr>
-      <td>Gesamtsumme:</td>
-      <td>{{ data.row.TotalAmount }}</td>
-    </tr>
-  </div>
-  `,
+  templateUrl: `balance-detail.component.html`,
   styleUrls: ['./balance-detail.component.css']
 })
 export class BalanceDetailComponent implements OnInit {
